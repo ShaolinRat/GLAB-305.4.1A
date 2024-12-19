a5b4757ec9ed55140927634eb47fa3ea2adb8e41
ShaolinRat <csprowalps@gmail.com>
2024-12-19 11:03:54 -0500

    Committing for lab 305.4.2A

--- src/main/java/com/test/project/HibernateIPABeginner/App.java
+++ src/main/java/com/test/project/HibernateIPABeginner/App.java
@@ -1,5 +1,7 @@
 package com.test.project.HibernateIPABeginner;
 
+import com.test.hib.controller.findUser_Hql;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        findUser_Hql u = new findUser_Hql();
+//        u.findUser();
+//        u.findUserSelect();
+//        u.getRecordbyId();
+//        u.getrecords();
+//        u.getmaxSalary();
+        u.getMaxSalaryGroupBy();
+       
     }
 }
